99375c9134ce9e3bc5eda8953a9036868220e5f8
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-02-01 23:46:20 +0200

    2121

--- JavaScript Essential/006_Практика/task1/lamp.js
+++ JavaScript Essential/006_Практика/task1/lamp.js
@@ -1,41 +1,41 @@
 let lamp = {
     isWorking: false,
+
     checkIsWorking: function () {
         while (!this.isWorking) {
-            // let isPluggedIn = confirm('Включена ли лампа?');
-
-            //     if (isPluggedIn) {
-            //         let bulbBurnedOut = confirm('Может лампочка перегорела?');
-            //         if (bulbBurnedOut) {
-            //             alert('Замените лампочку');
-            //             this.isWorking = confirm('Уже работает?');
-            //             if (!this.isWorking) {
-            //                 alert('Почините лампу');
-            //                 break;
-            //             }
-            //         } else {
-            //             alert('Почините лампу');
-            //             break;
-            //         }
-            //     } else {
-            //         alert('Включите лампу');
-            //         this.isWorking = confirm('Уже работает?');
-            //     }
-            // }
-            // if (this.isWorking) {
-            //     alert('Отлично! Работает!');
-            // }
             let isPluggedIn = confirm('Включена ли лампа?');
-            isPluggedIn ? bulbBurnedOut = confirm('Может лампочка перегорела?') : bulbBurnedOut ? (alert('Отлично! Работает!'), this.isWorking = confirm('Уже работает?')) : 
-            alert('Почините лампу');
-             
 
+            if (isPluggedIn) {
+                confirm('Может лампочка перегорела?') ? (alert('Замените лампочку'),this.isWorking = confirm('Уже работает?') ? 
+                alert('Включите лампу') : alert('Почините лампу')) : alert('Почините лампу');
 
+                // if (bulbBurnedOut) {
+                //     alert('Замените лампочку');
+                //     this.isWorking = confirm('Уже работает?');
+                //     if (!this.isWorking) {
+                //         alert('Почините лампу');
+                //         break;
+                //     }
+                // } else {
+                //     alert('Почините лампу');
+                //     break;
+                // }
 
+            } else {
+                alert('Включите лампу');
+                this.isWorking = confirm('Уже работает?');
+            }
         }
         if (this.isWorking) {
             alert('Отлично! Работает!');
         }
+
+
+
+
+
+
+
 
     }
 }
