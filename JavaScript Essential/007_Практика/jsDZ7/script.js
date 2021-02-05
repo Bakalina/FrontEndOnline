b8a48c330b910c8cbd08222b87f2020ee27b1fbd
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-02-05 22:25:22 +0200

    jsDZ7-1

--- JavaScript Essential/007_Практика/jsDZ7/script.js
+++ JavaScript Essential/007_Практика/jsDZ7/script.js
@@ -3,15 +3,28 @@ let fivetenQuestion = {
 
     game: function () {
         let counter = 0;
-        start = confirm('Давай ссыграем?');
+        let start = confirm('Давай ссыграем?');
         if (start == true) {
             alert('ПРАВДА или ЛОЖЬ')
-            oneQuestion = confirm('1) Левое полушарие мозга отвечает за логику, а правое — за творчество ?');
+            let oneQuestion = confirm('Левое полушарие мозга отвечает за логику, а правое — за творчество ?');
             if (oneQuestion == true) {
                 alert('Ответ не верный');
+                let twoQuestion = confirm('Земля — круглая?');
+                if (twoQuestion == true) {
+                    alert('Ответ не верный');
+                    let threeQuestion = confirm('Эверест — самая высокая гора в мире');
+                    threeQuestion == true?alert('Нет, хотя это очень частое заблуждение.'):(alert('Если брать все возвышенности на Земле, включая их скрытую под водой часть, то самая высокая гора в мире — это гавайская Мануа-Кеа'), counter++);
+                }
+                else {
+                    alert('Невероятно, но факт. Земля не круглая.');
+                    counter++;
+                    let fourQuestion = confirm('Банан — это ягода, а клубника — нет');
+                    fourQuestion == true?(alert('Да, это правда.'), counter++):alert('Удивитесь, но это все-таки правда.');
+                }                
             } else {
                 alert('Да, это просто миф.');
                 counter ++; 
+
             }
         } else {
             alert('Good Buy');
