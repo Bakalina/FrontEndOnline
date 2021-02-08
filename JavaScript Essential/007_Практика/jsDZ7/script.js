81aa559b8e6d0521331be834d14e79c93533ea3c
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-02-08 22:25:52 +0200

    jsDZ7-Final

--- JavaScript Essential/007_Практика/jsDZ7/script.js
+++ JavaScript Essential/007_Практика/jsDZ7/script.js
@@ -7,7 +7,7 @@ let questions = {
         let start = confirm('Давай ссыграем?');
         if (start == true) {
             alert('ПРАВДА или ЛОЖЬ')
-            let oneQuestion = confirm('Левое полушарие мозга отвечает за логику, а правое — за творчество ?');
+            let oneQuestion = confirm('Левое полушарие мозга отвечает за логику, а правое — за творчество?');
             if (oneQuestion == true) {
                 alert('Ответ не верный');
                 counterFalse++;
@@ -15,12 +15,12 @@ let questions = {
                 if (twoQuestion == true) {
                     alert('Ответ не верный');
                     counterFalse++;
-                    let threeQuestion = confirm('Эверест - самая большая гора');
+                    let threeQuestion = confirm('Эверест - самая большая гора?');
                     threeQuestion == true ? (alert('Ответ не верный'), counterFalse++) : (alert('Если брать все возвышенности на Земле, включая их скрытую под водой часть, то самая высокая гора в мире — это гавайская Мануа-Кеа'), counterTruth++);
                 } else {
                     alert('Невероятно, но факт. Земля не круглая.');
                     counterTruth++;
-                    let fourQuestion = confirm('Банан — это ягода, а клубника — нет');
+                    let fourQuestion = confirm('Банан — это ягода, а клубника — нет?');
                     fourQuestion == true ? (alert('Да, это правда.'), counterTruth++) : (alert('Ответ не верный'), counterFalse++);
                 }
             } else {
@@ -30,22 +30,22 @@ let questions = {
                 if (fiveQuestion == true) {
                     alert('Ответ не верный');
                     counterFalse++;
-                    let eightQuestion = confirm('Бумажный пакет — экологичнее, чем пластиковый');
+                    let eightQuestion = confirm('Бумажный пакет — экологичнее, чем пластиковый?');
                     eightQuestion == true ? (alert('Ответ не верный'), counterFalse++) : (alert('Вы правы, бумажный пакет, на самом деле, не экологичен. При производстве бумажного пакета выделяется на 70% больше CO2, чем при производстве пластикового.'), counterTruth++);
                 } else {
                     alert('Действительно, это ложь, пластик — не основной загрязнитель окружающей среды.');
                     counterTruth++;
                     if (counterTruth > counterFalse) {
-                        let sixQuestion = confirm('У рыб отсутствует долгосрочная память');
+                        let sixQuestion = confirm('У рыб отсутствует долгосрочная память?');
                         sixQuestion == true ? (alert('Ответ не верный'), counterFalse++) : (alert('Действительно, трехсекундная память золотой рыбки — это всего лишь миф.'), counterTruth++);
                     } else {
-                        let sevenQuestion = confirm('Алкоголь вреден для мозга');
+                        let sevenQuestion = confirm('Алкоголь вреден для мозга?');
                         sevenQuestion == true ? (alert('Ответ не верный'), counterFalse++) : (alert('Нет, это не так. Алкоголь крайне негативно воздействует на наш организм, особенно при частом употреблении, однако он не способен «убить» мозговые клетки.'), counterTruth++);
                     }
                 }
 
             }
-            
+
             counterTruth > counterFalse ? alert('Ураааа Вы выиграли!!!') : alert('Вы проиграли!!!');
             
         } else {
