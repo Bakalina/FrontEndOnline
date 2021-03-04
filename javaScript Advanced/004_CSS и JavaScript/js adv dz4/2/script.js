494b6b08363aaacd5aa99aee7794c47efb970f46
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-03-04 23:59:40 +0200

    js adv 8

--- javaScript Advanced/004_CSS и JavaScript/js adv dz4/2/script.js
+++ javaScript Advanced/004_CSS и JavaScript/js adv dz4/2/script.js
@@ -1,28 +1,43 @@
-window.onload = function () {
-    let button = document.getElementsByTagName('button')[0];
+window.addEventListener("load", init, false);
 
+function init() {
+    login.onchange = nameOnChange;
+    pass.onchange = passOnChahge;
+    form.onchange = formOnChahge;
+}
 
-    button.onclick = function () {
-        let login = document.getElementsByTagName('input')[0];
-        let pasworld = document.getElementsByTagName('input')[1];
-        login.style.backgroundColor = "red";
-        pasworld.style.backgroundColor = "red";
-    };
+function validate(elem, pattern) {
+    let res = elem.value.search(pattern);
+    if (res == -1) elem.className = "invalid";
+    else elem.className = "valid";
+}
 
-    let login = document.getElementsByTagName('input')[0];
+function nameOnChange() {
+    let pattern = /\w/;
+    validate(this, pattern);
+}
 
-    login.addEventListener = function () {
-        if (login.validity.typeMismatch) {
-            login.setCustomValidity('rere');
-        } else {
-            login.setCustomValidity('');
-        }
-    }
-
-    
-
+function passOnChahge() {
+    let pattern = /\w/;
+    validate(this, pattern);
 }
 
 
+function formOnChahge() {
+
+    let invalid = false;
+
+    for (let i = 0; i < form.elements.length; ++i) {
+        let e = form.elements[i];
+        if (e.type == "text" || e.type == "password" && e.onchange) {
+            e.onchange();
+            if (e.className == "invalid") invalid = true;
+        }
+    }
 
 
+    if (invalid) {
+        alert("Вы не заполнили поля логин и пароль!");
+        return false;
+    }
+}
