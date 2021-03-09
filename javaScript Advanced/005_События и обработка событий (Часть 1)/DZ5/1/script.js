f6bca1c2dc29ba38a121cdc0ad92e7a0e48611b5
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-03-09 23:22:56 +0200

    js adv dz5-2

--- javaScript Advanced/005_События и обработка событий (Часть 1)/DZ5/1/script.js
+++ javaScript Advanced/005_События и обработка событий (Часть 1)/DZ5/1/script.js
@@ -1,41 +1,110 @@
-
 // DOM level0
+
+// let plus = document.getElementById("plus");
+// let minus = document.getElementById("minus");
+// let multiply = document.getElementById("multiply");
+// let toSplit = document.getElementById("toSplit");
+// let operator;
+
+// function calc() {
+//     let operandOne = Number(document.getElementById("operandOne").value);
+//     let operandTwo = Number(document.getElementById("operandTwo").value);
+//     let result = 0;
+
+//     switch (operator) {
+//         case '+':
+//             result = operandOne + operandTwo;
+//             break;
+//         case '-':
+//             result = operandOne - operandTwo;
+//             break;
+//         case '*':
+//             result = operandOne * operandTwo;
+//             break;
+//         case '/':
+//             if (operandTwo != 0) {
+//                 result = operandOne / operandTwo;
+//             } else {
+//                 result = "Бесконечность"
+//             }
+//             break;
+//     }
+
+//     document.getElementById("result").innerHTML = result;
+// }
+
+// plus.onclick = function () {
+//     operator = "+";
+//     calc();
+// }
+
+// minus.onclick = function () {
+//     operator = "-";
+//     calc();
+// }
+
+// multiply.onclick = function () {
+//     operator = "*";
+//     calc();
+// }
+
+// toSplit.onclick = function () {
+//     operator = "/";
+//     calc();
+// }
+
+// -------------------------------------------------------------------------------------
+
+
+
+// DOM level2
 
 let plus = document.getElementById("plus");
 let minus = document.getElementById("minus");
 let multiply = document.getElementById("multiply");
 let toSplit = document.getElementById("toSplit");
-let result = document.getElementById("result");
+let operator;
 
-plus.onclick = function () {
+function calc() {
     let operandOne = Number(document.getElementById("operandOne").value);
     let operandTwo = Number(document.getElementById("operandTwo").value);
+    let result = 0;
 
-    let res = operandOne + operandTwo;
-    result.textContent = res;
+    switch (operator) {
+        case '+':
+            result = operandOne + operandTwo;
+            break;
+        case '-':
+            result = operandOne - operandTwo;
+            break;
+        case '*':
+            result = operandOne * operandTwo;
+            break;
+        case '/':
+            if (operandTwo != 0) {
+                result = operandOne / operandTwo;
+            } else {
+                result = "Бесконечность"
+            }
+            break;
+    }
 
+    document.getElementById("result").innerHTML = result;
 }
-minus.onclick = function () {
-    let operandOne = Number(document.getElementById("operandOne").value);
-    let operandTwo = Number(document.getElementById("operandTwo").value);
 
-    let res = operandOne - operandTwo;
-    result.textContent = res;
-
-}
-multiply.onclick = function () {
-    let operandOne = Number(document.getElementById("operandOne").value);
-    let operandTwo = Number(document.getElementById("operandTwo").value);
-
-    let res = operandOne * operandTwo;
-    result.textContent = res;
-
-}
-toSplit.onclick = function () {
-    let operandOne = Number(document.getElementById("operandOne").value);
-    let operandTwo = Number(document.getElementById("operandTwo").value);
-
-    let res = operandOne / operandTwo;
-    result.textContent = res;
-
-}
+plus.addEventListener("click", function () {
+    operator = "+";
+    calc();
+})
+minus.addEventListener("click", function () {
+    operator = "-";
+    calc();
+})
+multiply.addEventListener("click", function () {
+    operator = "*";
+    calc();
+})
+toSplit.addEventListener("click", function () {
+    operator = "/";
+    calc();
+})
