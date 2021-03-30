c5cb2000f251b97d3bc62057c2c7e43c17d04772
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-03-30 12:18:56 +0300

    html adv dz2-1

---
+++ HTML@CSS Advanced/002_Микроданные и пользовательские данные. Геолокация/dz2/script.js
@@ -0,0 +1,12 @@
+
+
+document.querySelector('.buttonBlueHaven').onclick = function() {
+    document.querySelector('.map').append()
+    document.querySelector('.map').style.visibility = 'visible'
+
+   
+}
+
+document.querySelector('.map').onclick = function() {
+    document.querySelector('.map').style.visibility = 'hidden'
+}
