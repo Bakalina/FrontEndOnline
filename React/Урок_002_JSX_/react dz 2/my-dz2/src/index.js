d710dc342aa238db758716c8be5ae37cced20c94
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-05-09 00:32:57 +0300

    react dz2.2

--- React/Урок_002_JSX_/react dz 2/my-dz2/src/index.js
+++ React/Урок_002_JSX_/react dz 2/my-dz2/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Text from './text';
+
+
+ReactDOM.render(
+    <Text />,
+document.getElementById('root')
+);
 
 
 
--- React/Урок_002_JSX_/react dz 2/my-dz2/src/text.js
+++ React/Урок_002_JSX_/react dz 2/my-dz2/src/text.js
@@ -1,3 +1,12 @@
 import React from "react";
 
 
+function  Text () {
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+}
+
+export default Text;
