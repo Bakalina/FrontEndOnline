1b5638fda03f5c80d55b64c414d0ef703fa33e46
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-05-17 00:22:31 +0300

    react dz3.3

--- React/Урок_003_Основы_React/react-dz3/src/App.js
+++ React/Урок_003_Основы_React/react-dz3/src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import Counter from "./counter/counter";
 import ListUsers from "./Users/Users";
 
-const users = ["Вася", "Петя", "Маша", "Петя"]
-const usersSet = new Set(users)
+const users = ["Вася", "Петя", "Маша", "Петя", "Вася", "Петя", "Маша", "Петя"]
+const userSort = users.filter((item ,index) => users.indexOf(item) === index)
 
 
 function App() {
     return (
         <div className='container'>
             <Counter/>
-            <ListUsers value={usersSet}/>
+            <ListUsers value={userSort}/>
         </div>
     )
 }
--- React/Урок_003_Основы_React/react-dz3/src/Users/Users.js
+++ React/Урок_003_Основы_React/react-dz3/src/Users/Users.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const ListUsers = (props) => {
-    const value = props.value;
 
+const ListUsers = (props) => {
+
+    const value = props.value
     const list = value.map((item) => <li key={item.toString()}>
         {item}
     </li>)
