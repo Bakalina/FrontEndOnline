ba00768e68842a35b561def47f0f1719e8a447ec
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-05-19 14:48:05 +0300

    react 6

---
+++ React/Урок_006_Стилизация/006_Samples/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Avatar } from "./components/avatar";
+import { Counter } from "./components/counter";
+import { firstListItems } from "./const/firstList";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <Avatar 
+        name="Ann"
+        twitter="link to tw"
+        facebook="link to fb"
+      />
+      <Counter />
+      <ul>
+        {firstListItems.map(item => <li key={item.id}>{item.text}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
