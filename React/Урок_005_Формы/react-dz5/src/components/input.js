5bc444f60af62f69b39cee6d8c1e53e6e92dda9d
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-05-23 12:35:41 +0300

    react dz5.3

--- React/Урок_005_Формы/react-dz5/src/components/input.js
+++ React/Урок_005_Формы/react-dz5/src/components/input.js
@@ -10,27 +10,38 @@ const TextInput = (props) => {
 class FormInput extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {text: ''}
+        this.state = {lastName: ''}
+        this.state = {firstName: ''}
+        this.state = {commentUser: ''}
     }
 
-    setText = (e) => {
-        this.setState({text: e.target.value})
+    setLastName = (e) => {
+        this.setState({lastName: e.target.value})
     }
-
+    setFirstName = (e) => {
+        this.setState({firstName: e.target.value})
+    }
+    setCommentUser = (e) => {
+        this.setState({commentUser: e.target.value})
+    }
 
     render() {
-        const textOne = this.state.text;
-        const textTwo = this.state.text;
 
         return (
             <form>
                 <input
-                    value={textOne}
-                    onChange={this.setText}>
+                    value={this.state.lastName}
+                    onChange={this.setLastName}>
                 </input>
-                <TextInput
-                    text={textOne}/>
+                <input
+                    value={this.state.firstName}
+                    onChange={this.setFirstName}>
+                </input>
+                <input
+                    value={this.state.commentUser}
+                    onChange={this.setCommentUser}>
+                </input>
+                <button style={{width:'50px', height:'20px'}}>send</button>
             </form>
         )
     }
