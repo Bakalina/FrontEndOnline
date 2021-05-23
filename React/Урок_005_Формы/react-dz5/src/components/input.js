799228e70ff67f9c8ad6b506d750748f15f4d446
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-05-23 10:23:23 +0300

    react dz5.2

--- React/Урок_005_Формы/react-dz5/src/components/input.js
+++ React/Урок_005_Формы/react-dz5/src/components/input.js
@@ -1,24 +1,45 @@
 import React from "react";
 
 
-class Form extends React.Component {
+const TextInput = (props) => {
+    return (
+        <p>{props.text}</p>
+    )
+}
+
+class FormInput extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {text: ''}
     }
 
-    setText(e) {
+    setText = (e) => {
         this.setState({text: e.target.value})
     }
 
 
     render() {
-        const text = this.state.text;
+        const textOne = this.state.text;
+        const textTwo = this.state.text;
+
         return (
-            <input
-                value={text}
-                onChange={this.setText}>
-            </input>
+            <form>
+                <input
+                    value={textOne}
+                    onChange={this.setText}>
+                </input>
+                <TextInput
+                    text={textOne}/>
+            </form>
+        )
+    }
+}
+
+class Form extends React.Component {
+    render() {
+        return (
+            <FormInput/>
         )
     }
 }
