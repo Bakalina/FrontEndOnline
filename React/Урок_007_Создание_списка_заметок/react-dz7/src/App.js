d06d1418a0cc812498cd12f329ec17e8ea327cc6
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-05-30 22:55:45 +0300

    react dz7.3

--- React/Урок_007_Создание_списка_заметок/react-dz7/src/App.js
+++ React/Урок_007_Создание_списка_заметок/react-dz7/src/App.js
@@ -1,8 +1,12 @@
+import './App.scss'
+import PlusTaskBtn from "./Components/Button/PlusTaskBtn";
+
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="app">
+      <h1>TODO list</h1>
+        <PlusTaskBtn/>
     </div>
   );
 }
---
+++ React/Урок_007_Создание_списка_заметок/react-dz7/src/Components/Button/PlusTaskBtn.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './PlusTaskBtn.scss';
+
+
+class PlusTaskBtn extends React.Component {
+    render() {
+        return (
+            <button>Plus</button>
+        )
+    }
+}
+
+export default PlusTaskBtn;
